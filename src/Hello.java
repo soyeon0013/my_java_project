6e01feb1c79b66db7876b149eb5ca62e928ea334
key <key@naver.com>
2025-07-22 15:32:00 +0900

    두번째 commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,8 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Hello {
     public static void main(String[] args) {
         System.out.printf("Hello~~~~월드!");
 
+        System.out.printf("하하하하하하");
     }
 }
